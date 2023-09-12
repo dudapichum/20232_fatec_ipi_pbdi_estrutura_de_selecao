f4ab3378a1d97ec23ef4fe53155895449de405c6
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-09-12 11:37:11 -0300

    ao09(selecao): validacao de datas
    - validacao de datas de formato ddmmaaaa com if/else e case/when

--- script.sql
+++ script.sql
@@ -1,21 +1,79 @@
---Outra maneira de usar o CASE
-DO 
-$$
-DECLARE 
-	valor INT := fn_valor_aleatorio_entre(1, 12);
-	mensagem VARCHAR(200);
+-- Dada uma data no formato ddmmaaaa verificar se ela é válida 
+--TESTAR:
+-- 22/10/2022: válida 
+-- 29/02/2020: 2020 é bissexto, válida 
+-- 29/02/2021: inválida
+-- 28/02/201: válida 
+-- 31/06/2021: inválida
+
+DO $$
+DECLARE
+	 data INT:= 31062021;
+	 -- quero exibir a data assim 31/06/2021
+	 dia INT;
+	 mes INT;
+	 ano INT;
+	 data_valida BOOL := TRUE;
 BEGIN
-	RAISE NOTICE'O valor é: %', valor;
-	CASE valor 
-		WHEN 1, 3, 5, 7, 9 THEN
-			RAISE NOTICE 'Ímpar';
-		WHEN 2, 4, 6, 8 THEN
-			RAISE NOTICE 'Par';
+	dia := data / 1000000;
+	mes := data % 1000000 / 10000;
+	ano := data % 10000;
+	RAISE NOTICE 'a data é %/%/%', dia, mes, ano;
+	IF ano < 1 THEN
+		RAISE NOTICE 'Data inválida';
+	ELSE
+		CASE
+			WHEN mes > 12 OR mes < 1 OR dia < 1 OR dia > 31 THEN
+				data_valida := FALSE;
+			ELSE
+				IF((mes = 4 OR mes = 6 OR mes = 9 OR mes = 11) AND dia > 30) THEN
+					data_valida:= FALSE;
+				ELSE 
+					-- fevereiro
+					IF mes = 2 THEN
+						CASE
+							--ano bissexto 
+							WHEN (ano % 4 = 0 AND ano % 100 <> 0) THEN
+								IF dia > 29 THEN
+									data_valida := FALSE;
+								END IF;
+							ELSE 
+								-- estamos em fev e o ano não é bissexto
+								IF dia > 28 THEN
+									data_valida := FALSE;
+								END IF;
+						END CASE;
+					END IF;
+				END IF;
+		END CASE;
+	END IF;
+	CASE 
+		WHEN data_valida THEN
+			RAISE NOTICE 'A data é válida';
 		ELSE
-			RAISE NOTICE 'Fora do Intervalo de tratamento';
+			RAISE NOTICE 'A data é inválida';
 	END CASE;
 END
 $$
+
+--Outra maneira de usar o CASE
+-- DO 
+-- $$
+-- DECLARE 
+-- 	valor INT := fn_valor_aleatorio_entre(1, 12);
+-- 	mensagem VARCHAR(200);
+-- BEGIN
+-- 	RAISE NOTICE'O valor é: %', valor;
+-- 	CASE valor 
+-- 		WHEN 1, 3, 5, 7, 9 THEN
+-- 			RAISE NOTICE 'Ímpar';
+-- 		WHEN 2, 4, 6, 8 THEN
+-- 			RAISE NOTICE 'Par';
+-- 		ELSE
+-- 			RAISE NOTICE 'Fora do Intervalo de tratamento';
+-- 	END CASE;
+-- END
+-- $$
 
 --CASE
 -- DO 
