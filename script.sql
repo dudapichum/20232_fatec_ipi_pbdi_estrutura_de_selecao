f3f2314a35da32da158c910ffaeb09007480e206
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-09-12 08:58:57 -0300

    ap09(selecao): exemplo com if simples

--- script.sql
+++ script.sql
@@ -1,7 +1,26 @@
-CREATE OR REPLACE FUNCTION fn_valor_aleatorio_entre 
-(lim_inferior INT, lim_superior INT) RETURNS INT AS
+DO 
 $$
+DECLARE 
+	 valor INT
 BEGIN
-	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
-END;
-$$ LANGUAGE plpgsql;
+	valor := fn_valor_aleatorio_entre(1, 10);
+	RAISE NOTICE 'O valor gerado foi %', valor;
+	
+	IF valor <= 20 THEN
+		RAISE NOTICE 'Sim, % é o menor ou igual a 20', valor;
+	END IF;
+END
+$$
+
+
+
+--SELECT fn_valor_aleatorio_entre(2, 5);
+
+
+-- CREATE OR REPLACE FUNCTION fn_valor_aleatorio_entre 
+-- (lim_inferior INT, lim_superior INT) RETURNS INT AS
+-- $$
+-- BEGIN
+-- 	RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+-- END;
+-- $$ LANGUAGE plpgsql;
